ecdd90e1135d38b57422be250d57153d58db4ba6
duranje <joeduran8@gmail.com>
2019-02-22 11:53:33 -0600

    minor changes

--- src/api/middlewares.tsx
+++ src/api/middlewares.tsx
@@ -1,6 +1,11 @@
 import { EbcFetchConfig } from './api';
 
 export const loggingMiddleware = ({ config, url }: EbcFetchConfig) => {
-  console.log(`EBC-API-${config ? config.method : 'GET'}:${url}`, config ? config.body : null);
-  return Promise.resolve({ config, url });
+	if (process.env.NODE_ENV === 'development') {
+		console.log(
+			`EBC-API-${config ? config.method : 'GET'}:${url}`,
+			config ? config.body : null
+		);
+	}
+	return Promise.resolve({ config, url });
 };
--- src/components/screens/stats/index.tsx
+++ src/components/screens/stats/index.tsx
@@ -3,19 +3,20 @@ import { getWorkouts } from '../../../api';
 import { Main } from '../../page';
 import { formattedTime } from '../../util';
 export default function Stats() {
-  const [workouts, setWorkouts] = useState<Array<any>>([]);
-  useEffect(() => {
-    getWorkouts().then(setWorkouts);
-  });
-  return (
-    <Main>
-      <h1>Stats</h1>
-      <div>
-        {workouts.map(workout => {
+	const [workouts, setWorkouts] = useState<Array<any>>([]);
+	useEffect(() => {
+		setWorkouts([{ endTime: 3000, startTime: 1000 }]);
+		// getWorkouts().then(setWorkouts);
+	},[]);
+	return (
+		<Main>
+			<h1>Stats</h1>
+			<div>
+				{workouts.map(workout => {
           const totalTime = workout.endTime - workout.startTime;
-          return <div>Total Time: {formattedTime(totalTime)}</div>;
-        })}
-      </div>
-    </Main>
-  );
+					return <div>Total Time: {formattedTime(totalTime)}</div>;
+				})}
+			</div>
+		</Main>
+	);
 }
